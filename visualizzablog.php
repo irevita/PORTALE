8f05ca7a9d0c353c0c235fe9e814b0bfc68d83c1
MarjackSJ <ml.santagati92@gmail.com>
2022-04-03 18:18:46 +0200

    last

--- visualizzablog.php
+++ visualizzablog.php
@@ -136,30 +136,31 @@
                         <h3><?php echo $row["Titolo"];?></h3>
                         <p><?php echo $row["TESTO"];?></p>
                     </article>
-                </div>    
+                  
+             
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <?php while($row = mysqli_fetch_array($query_immagini)) { ?>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="<?php $prev = $row["Nome"].''; echo $row["Nome"]; ?>" alt="First slide">
+                            </div>
+                            <?php } ?>
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="<?php echo ($prev); ?>" alt="First slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>   
             <?php } ?>
-        </div>   
-
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <?php while($row = mysqli_fetch_array($query_immagini)) { ?>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="<?php $prev = $row["Nome"].''; echo $row["Nome"]; ?>" alt="First slide">
-                </div>
-                <?php } ?>
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="<?php echo ($prev); ?>" alt="First slide">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+        </div>  
 
         <?php 
             if (isset($_POST['infoblog'])){
